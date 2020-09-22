16faf18e39359dc2ea269836806d0770f7ced763
Elbert Alias <77259+AliasIO@users.noreply.github.com>
2020-09-23 07:21:27 +1000

    Revert to Drift

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,46 +21,11 @@
       src="https://r.wdfl.co/rw.js"
       data-rewardful="7fd1cb"
     ></script>
-
-    <script
-      async
-      src="https://sc.lfeeder.com/lftracker_v1_lYNOR8xM2GG7WQJZ.js"
-    ></script>
-
-    <script>
-      window.pipedriveLeadboosterConfig = {
-        base: 'leadbooster-chat.pipedrive.com',
-        companyId: 7691475,
-        playbookUuid: '635f25e4-8da7-40bd-b3a0-1497505ef443',
-        version: 2,
-      }
-      ;(function () {
-        var w = window
-        if (w.LeadBooster) {
-          console.warn('LeadBooster already exists')
-        } else {
-          w.LeadBooster = {
-            q: [],
-            on: function (n, h) {
-              this.q.push({ t: 'o', n: n, h: h })
-            },
-            trigger: function (n) {
-              this.q.push({ t: 't', n: n })
-            },
-          }
-        }
-      })()
-    </script>
-
-    <script
-      src="https://leadbooster-chat.pipedrive.com/assets/loader.js"
-      async
-    ></script>
   </v-app>
 </template>
 
 <script>
-// /* globals drift */
+/* globals drift */
 
 import { mapState, mapActions } from 'vuex'
 
@@ -70,7 +35,6 @@ import Drawer from '~/components/Drawer.vue'
 import mainNav from '~/assets/json/nav/main.json'
 import userNav from '~/assets/json/nav/user.json'
 
-/*
 const driftRoutes = {
   '/about': null,
   '/account': null,
@@ -87,7 +51,6 @@ const driftRoutes = {
   '/paymentmethods': null,
   '/subscriptions': null,
 }
-*/
 
 export default {
   components: {
@@ -98,9 +61,9 @@ export default {
   data() {
     return {
       mainNav,
-      // driftShown: false,
-      // driftApi: null,
-      // driftIsOnline: false,
+      driftShown: false,
+      driftApi: null,
+      driftIsOnline: false,
     }
   },
   computed: {
@@ -119,23 +82,20 @@ export default {
       return this.isSignedIn ? userNav : userNav.filter((item) => !item.auth)
     },
   },
-  /*
   watch: {
     $route() {
       this.checkDrift()
     },
   },
-  */
   mounted() {
     this.updateUserAttrs()
 
-    // this.checkDrift()
+    this.checkDrift()
   },
   methods: {
     ...mapActions({
       updateUserAttrs: 'user/updateAttrs',
     }),
-    /*
     initDrift() {
       if (typeof window === 'undefined') {
         return
@@ -253,7 +213,6 @@ export default {
         this.driftApi.widget.show()
       }
     },
-    */
   },
 }
 </script>
