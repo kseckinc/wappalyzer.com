4bd8c845771ec7336370ba7b5615d62fe8aed0fd
Elbert Alias <77259+AliasIO@users.noreply.github.com>
2020-10-30 19:36:47 +1100

    New technology lookup

--- pages/lookup/_url.vue
+++ pages/lookup/_url.vue
@@ -84,6 +84,17 @@
       </v-card-text>
     </v-card>
 
+    <p>
+      <small>
+        Look up 5,000 websites at once with
+        <nuxt-link to="/bulk/">Bulk lookup</nuxt-link>.<br />
+        Automate lookups with the
+        <nuxt-link to="/api/">Lookup API</nuxt-link>.<br />
+        Get the free
+        <nuxt-link to="/download/">browser extension</nuxt-link>.
+      </small>
+    </p>
+
     <v-dialog v-model="signInDialog" max-width="400px">
       <SignIn mode-sign-up mode-continue />
     </v-dialog>
