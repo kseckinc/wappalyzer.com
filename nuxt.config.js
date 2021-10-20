0d3b43f6738423cb9ad075264c8ffaef3ef3080d
Elbert Alias <77259+AliasIO@users.noreply.github.com>
2021-10-21 08:01:09 +1100

    Update API paths

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,9 +6,9 @@ const publicRuntimeConfig = {
   COGNITO_CLIENT_ID: '17uprj6843uiev15qqdkn3l5h7',
   COGNITO_IDENTITY_POOL_ID:
     'ap-southeast-2:6b7b7db4-96fd-450f-88fc-2a6feb55cb8e',
-  BASE_URL: 'https://api2.wappalyzer.com/',
-  DATASETS_BASE_URL: 'https://datasets.wappalyzer.com/',
-  BULK_LOOKUP_BASE_URL: 'https://bulk.wappalyzer.com/',
+  BASE_URL: 'https://api.wappalyzer.com/',
+  DATASETS_BASE_URL: 'https://lists.wappalyzer.com/',
+  BULK_LOOKUP_BASE_URL: 'https://lookup.wappalyzer.com/',
   RELEASE_URL:
     'https://s3.dualstack.ap-southeast-2.amazonaws.com/assets.wappalyzer.com/release.json',
   ...(process.env.NODE_ENV === 'development' ||
